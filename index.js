05c36cf22dd722df4712df903a19c4c63f5d1ae9
Xuefeng-Zhu <xzhu15@illinois.edu>
2014-09-20 01:26:48 -0500

    init commit

---
+++ index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var request = require('request');
+var app = express();
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+ });
+
+ app.get('/image/:item', function(req, res, next){
+ 	request('http://api.duckduckgo.com/?q=ipad&format=json&pretty=1', function(error, response, body){
+ 		res.send(body);
+ 	})
+ });
+
+app.listen(5000);
